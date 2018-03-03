348eb32e0d6b3502da32f963f4c65a5a3e7ff4f3
nashaofu <diaocheng@outlook.com>
2018-03-03 16:36:02 +0800

    使用typescript编写，修改clear方法逻辑

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # electron-dev-webpack-plugin
 A webpack plugin for electron development, When the file changes automatically restart electron main process. example: [shortcut-capture](https://github.com/nashaofu/shortcut-capture/blob/master/build/main/webpack.dev.conf.js#L15)
 
+## Install
+
+[![NPM](https://nodei.co/npm/electron-dev-webpack-plugin.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/electron-dev-webpack-plugin/)
+
 ## Usage
 ```js
 new ElectronDevWebpackPlugin()
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+import chalk from 'chalk'
+import { ChildProcess, spawn } from 'child_process'
+import electron from 'electron'
+import portfinder from 'portfinder'
+
+declare module 'child_process' {
+  function spawn (command: string | electron.AllElectron, args?: string[], options?: SpawnOptions): ChildProcess
+}
+
+export = class ElectronDevWebpackPlugin {
+  private port: number
+  private process: ChildProcess[] = []
+  private timer?: NodeJS.Timer
+
+  constructor ({
+    port = 5858 // electron inspect端口
+  } = {}) {
+    this.port = port
+  }
+
+  /**
+   * webpack调用接口
+   * @param {*} compiler
+   */
+  public apply (compiler: any) {
+    portfinder.basePort = this.port
+    compiler.plugin('done', () => {
+      portfinder.getPortPromise()
+        .then(port => this.spawn(port))
+        .catch(err => this.spawn())
+    })
+  }
+
+  /**
+   * 启动新进程
+   * @param {Number|undefined} port
+   */
+  private spawn (port?: number) {
+    this.clear()
+      .then(() => {
+        const args = typeof port === 'number'
+          ? [`--inspect=${port}`, '.']
+          : ['.']
+        const cp: ChildProcess = spawn(electron, args)
+
+        cp.stdout.on('data', data => {
+          this.log(chalk.yellowBright.bold.strikethrough(data.toString()))
+        })
+        cp.stderr.on('data', data => {
+          this.log(chalk.redBright.bold.strikethrough(data.toString()))
+        })
+        this.process.push(cp)
+      })
+  }
+
+  /**
+   * 清理旧进程
+   */
+  private clear (): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const next = () => {
+        this.kill()
+        // 检查旧进程，防止没有被清理掉
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        if (this.process.length) {
+          this.timer = setTimeout(() => next(), 1000)
+        } else {
+          resolve()
+        }
+      }
+      next()
+    })
+  }
+
+  /**
+   * 杀掉进程
+   */
+  private kill () {
+    this.process = this.process.reduce((p: ChildProcess[], cp: ChildProcess) => {
+      if (!cp.killed) {
+        try {
+          cp.kill()
+        } catch (e) {
+          console.log(`kill ${chalk.red(cp.pid.toString())} process is failed, ${chalk.red(e)}`)
+        }
+      }
+      if (!cp.killed) {
+        p.push(cp)
+      }
+      return p
+    }, [])
+  }
+
+  /**
+   * 打印主进程输出
+   * @param {*} data
+   */
+  private log (data: string) {
+    console.log('------------Main Process Log Start------------')
+    console.log(data)
+    console.log('-------------Main Process Log End-------------')
+  }
+}
